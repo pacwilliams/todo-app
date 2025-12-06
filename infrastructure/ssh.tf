77571b3e9bb6898daa03806957b4407edec34385
pacwilliams <pacwilliams@hotmail.com>
2025-12-06 12:25:49 -0500

    feat(ssh): add role assignments for Key Vault access

--- infrastructure/ssh.tf
+++ infrastructure/ssh.tf
@@ -21,6 +21,18 @@ resource "azurerm_key_vault" "kv" {
   }
 }
 
+resource "azurerm_role_assignment" "kv_secrets_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = "8659ba1e-7d54-46a4-975b-9aebf6a33a57" 
+}
+
+resource "azurerm_role_assignment" "sp_kv_secrets_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 resource "azapi_resource_action" "ssh_public_key_gen" {
   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   resource_id = azapi_resource.ssh_public_key.id
@@ -29,7 +41,7 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 
   response_export_values = ["publicKey", "privateKey"]
 
-  depends_on = ["azurerm_key_vault.kv"]
+  depends_on = [azurerm_key_vault.kv]
 }
 
 resource "azapi_resource" "ssh_public_key" {
@@ -38,7 +50,7 @@ resource "azapi_resource" "ssh_public_key" {
   location  = azurerm_resource_group.rg.location
   parent_id = azurerm_resource_group.rg.id
 
-  depends_on = ["azurerm_key_vault.kv"]
+  depends_on = [azurerm_key_vault.kv]
 }
 
 # Store the generated public key
