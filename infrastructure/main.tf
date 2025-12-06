284986ec90b3fcef9e0f19cda98f7a48cf544e83
pacwilliams <pacwilliams@hotmail.com>
2025-12-06 13:51:43 -0500

    feat(dns): add DNS zone and A record for todoapp

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -182,6 +182,37 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
 }
 
+resource "azurerm_dns_zone" "main" {
+  name                = "pwwizexercise.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_dns_a_record" "backend_dns" {
+  name                = "todoapp"
+  zone_name           = azurerm_dns_zone.main.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  records             = [azurerm_public_ip.lb.ip_address]
+}
+
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefixes    = ["*"] # e.g. corporate IPs
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = "${azurerm_resource_group.rg.name}-aks"
   location            = azurerm_resource_group.rg.location
