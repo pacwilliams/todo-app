bc650aefa43667dbadaed3b4ad58a0112d87cdfa
pacwilliams <pacwilliams@hotmail.com>
2025-12-06 11:09:01 -0500

    Add SSH Keys to Keyvault

--- infrastructure/ssh.tf
+++ infrastructure/ssh.tf
@@ -3,6 +3,24 @@ resource "random_pet" "ssh_key_name" {
   separator = ""
 }
 
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "${azurerm_resource_group.rg.name}-KeyVault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions    = ["get", "list", "create", "delete"]
+    secret_permissions = ["get", "list", "set", "delete"]
+  }
+}
+
 resource "azapi_resource_action" "ssh_public_key_gen" {
   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   resource_id = azapi_resource.ssh_public_key.id
@@ -19,6 +37,16 @@ resource "azapi_resource" "ssh_public_key" {
   parent_id = azurerm_resource_group.rg.id
 }
 
-output "key_data" {
-  value = azapi_resource_action.ssh_public_key_gen.output.publicKey
+# Store the generated public key
+resource "azurerm_key_vault_secret" "ssh_public_key" {
+  name         = "aks-ssh-public-key"
+  value        = azapi_resource_action.ssh_public_key_gen.output.publicKey
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+# Store the generated private key
+resource "azurerm_key_vault_secret" "ssh_private_key" {
+  name         = "aks-ssh-private-key"
+  value        = azapi_resource_action.ssh_public_key_gen.output.privateKey
+  key_vault_id = azurerm_key_vault.kv.id
 }
