0ab50b42f61ebcdac890cb2c32f4d45ae1e51317
pacwilliams <pacwilliams@hotmail.com>
2025-12-06 10:27:29 -0500

    Add AKS to custom subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,6 +17,14 @@ resource "azurerm_subnet" "subnet_1" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "aks-delegation"
+    service_delegation {
+      name    = "Microsoft.ContainerService/managedClusters"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
 }
 
 resource "azurerm_subnet" "subnet_2" {
@@ -39,7 +47,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "${azurerm_resource_group.rg.name}-ExternalNSG"
+  name                = "${azurerm_resource_group.rg.name}-NSG"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -189,15 +197,15 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   dns_prefix          = "pwakscluster"
 
   default_node_pool {
-    type                 = "VirtualMachineScaleSets"
-    name                 = "default"
-    node_count           = 1
-    max_count            = 3
-    min_count            = 1
-    vm_size              = "Standard_DS2_v2"
-    auto_scaling_enabled = true
+    type                   = "VirtualMachineScaleSets"
+    name                   = "default"
+    node_count             = 1
+    max_count              = 3
+    min_count              = 1
+    vm_size                = "Standard_DS2_v2"
+    auto_scaling_enabled   = true
     node_public_ip_enabled = false
-    vnet_subnet_id = azurerm_subnet.subnet_1.id
+    vnet_subnet_id         = azurerm_subnet.subnet_1.id
     upgrade_settings {
       drain_timeout_in_minutes      = 90
       max_surge                     = "10%"
