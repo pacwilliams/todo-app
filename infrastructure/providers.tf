8f6b8aef4337dc8be3ae7d75dc59b40af450f88c
pacwilliams <pacwilliams@hotmail.com>
2025-12-09 12:12:17 -0500

    Add username and password authentication

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -50,16 +50,8 @@ provider "kubernetes" {
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].cluster_ca_certificate)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "az"
-    args        = [
-      "aks", "get-credentials",
-      "--resource-group", azurerm_kubernetes_cluster.aks_cluster.resource_group_name,
-      "--name", azurerm_kubernetes_cluster.aks_cluster.name,
-      "--overwrite-existing"
-    ]
-  }
+  username               = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].username
+  password               = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].password
 
 }
 
@@ -74,7 +66,7 @@ provider "helm" {
 
 provider "grafana" {
   alias = "main"
-    url = "http://${coalesce(
+  url = "http://${coalesce(
     data.kubernetes_service_v1.grafana.status[0].load_balancer[0].ingress[0].ip,
     data.kubernetes_service_v1.grafana.status[0].load_balancer[0].ingress[0].hostname
   )}:3000"
