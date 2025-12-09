3ed6bf800603ce5b9869d822e5482880f8bf88dd
pacwilliams <pacwilliams@hotmail.com>
2025-12-09 10:10:49 -0500

    Update Providers file

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -50,8 +50,9 @@ provider "kubernetes" {
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].cluster_ca_certificate)
-  username               = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].username
-  password               = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].password
+  username               = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config[0].username
+  password               = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config[0].password
+
 }
 
 provider "helm" {
