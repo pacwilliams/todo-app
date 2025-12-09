83751a9f2d93281bd3776e10c2778eaed7d609dc
pacwilliams <pacwilliams@hotmail.com>
2025-12-09 12:23:47 -0500

    change authentication

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -47,9 +47,12 @@ provider "azurerm" {
 
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_key)
+  #client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_certificate)
+  #client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].cluster_ca_certificate)
+  username               = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].username
+  password               = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].password
+
 }
 
 provider "helm" {
