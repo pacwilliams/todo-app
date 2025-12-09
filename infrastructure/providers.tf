d3b4bdaf77b4d9a16a1a59f603d1b1714b353c9c
pacwilliams <pacwilliams@hotmail.com>
2025-12-09 09:50:15 -0500

    Added token to Kubernetes provider

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -50,6 +50,7 @@ provider "kubernetes" {
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config[0].cluster_ca_certificate)
+  token = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].token
 }
 
 provider "helm" {
