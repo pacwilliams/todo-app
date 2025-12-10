ea3c72042d5bb99d3a38e4d9067eb901e0acd67d
pacwilliams <pacwilliams@hotmail.com>
2025-12-09 23:04:12 -0500

    Update outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -15,18 +15,6 @@ output "public_ip_address" {
   value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
 
-output "aks_ingress_public_ip" {
-  value = azurerm_public_ip.aks_ingress_ip.ip_address
-}
-
-output "aks_ingress_public_fqdn" {
-  value = azurerm_public_ip.aks_ingress_ip.fqdn
-}
-
-output "aks_ingress_custom_domain" {
-  value = azurerm_dns_a_record.aks_dns.fqdn
-}
-
 output "grafana_lb_ip" {
   value = data.kubernetes_service_v1.grafana.status[0].load_balancer[0].ingress[0].ip
 }
