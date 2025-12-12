0934986992ad04978c56db8e6e605f5a25095287
pacwilliams <pacwilliams@hotmail.com>
2025-12-12 18:27:36 -0500

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,22 +4,10 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
-variable "resource_group_name_prefix" {
-  type        = string
-  default     = "ODL-candidate-sandbox-02-1986716"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-
 variable "username" {
   type        = string
   description = "The username for the local account that will be created on the new VM."
   default     = "azureadmin"
-}
-
-variable "enable_manifests" {
-  type        = bool
-  description = "Enable or disable the deployment of Kubernetes manifests."
-  default     = true
 }
 
 variable "api_token" {
