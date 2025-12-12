052894e43f7c17dc8acdea7126f017fa51275a2a
pacwilliams <pacwilliams@hotmail.com>
2025-12-11 19:59:01 -0500

    Uncomment Cloudflare resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -336,16 +336,16 @@ data "kubernetes_service_v1" "nginx_ingress" {
   }
 }
 
-# resource "cloudflare_dns_record" "todo" {
-#   zone_id = var.zone_id
-#   name    = "*"
-#   content = data.kubernetes_service_v1.nginx_ingress.status[0].load_balancer[0].ingress[0].ip
-#   type    = "A"
-#   ttl     = 3600
-#   proxied = false
+resource "cloudflare_dns_record" "todo" {
+  zone_id = var.zone_id
+  name    = "*"
+  content = data.kubernetes_service_v1.nginx_ingress.status[0].load_balancer[0].ingress[0].ip
+  type    = "A"
+  ttl     = 3600
+  proxied = false
 
-#   depends_on = [helm_release.nginx_ingress]
-# }
+  depends_on = [helm_release.nginx_ingress]
+}
 
 # Helm release for Cert-Manager
 resource "helm_release" "cert_manager" {
